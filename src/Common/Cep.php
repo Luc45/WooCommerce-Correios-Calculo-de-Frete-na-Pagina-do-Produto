cd8c1ee35d41c4a3f161684c721e4f2259e186d5
Lucas Bustamante <lucasfbustamante@gmail.com>
2018-09-28 15:13:10 -0300

    Moved CEP validation to Validation class

--- src/Common/Cep.php
+++ src/Common/Cep.php
@@ -13,16 +13,4 @@ class Cep {
         return defined('CFPP_CEP') ? CFPP_CEP : get_option( 'woocommerce_store_postcode' );
     }
 
-    /**
-     * Checks if given CEP is valid
-     *
-     *  @param string $cep Cep to check if valid
-     *  @return boolean
-     */
-    public function isValid(string $cep)
-    {
-        $cep = preg_replace('/[^0-9]/', '', $cep);
-        return strlen($cep) == 8;
-    }
-
 }
