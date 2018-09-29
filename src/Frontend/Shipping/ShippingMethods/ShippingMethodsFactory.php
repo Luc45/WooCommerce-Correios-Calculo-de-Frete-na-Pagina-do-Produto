49f67ce64dad75b7f604348dd4ab636a79c83d50
Lucas Bustamante <lucasfbustamante@gmail.com>
2018-09-28 21:07:14 -0300

    Shipping Method Factory

---
+++ src/Frontend/Shipping/ShippingMethods/ShippingMethodsFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CFPP\Frontend\Shipping\ShippingMethods;
+
+class ShippingMethodsFactory {
+
+    /**
+    *   Returns a Shipping Method class
+    */
+    public function getClass(string $shipping_method) {
+        $class = $shipping_method.'_Shipping_Method';
+        $file = __DIR__.'/Methods/'.$class.'.php';
+
+        if (!file_exists($file))
+            return false;
+
+        include_once($file);
+        return new $class;
+    }
+
+}
