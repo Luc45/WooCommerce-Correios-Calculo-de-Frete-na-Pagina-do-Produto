ae3a6a45ef4d58904bec79dd889ecf9b072f092f
Lucas Bustamante <lucasfbustamante@gmail.com>
2018-09-30 19:32:53 -0300

    Free Shipping implementation

--- src/Frontend/Shipping/ShippingMethods/Methods/WC_Shipping_Free_Shipping_Shipping_Method.php
+++ src/Frontend/Shipping/ShippingMethods/Methods/WC_Shipping_Free_Shipping_Shipping_Method.php
@@ -15,12 +15,27 @@ class WC_Shipping_Free_Shipping_Shipping_Method extends ShippingMethodsAbstract 
     /**
     *   Receives a Request and calculates the shipping
     */
-    public function calculate(array $request) {
-        $request['produto_altura'];
-        $request['produto_largura'];
-        $request['produto_comprimento'];
-        $request['produto_peso'];
-        $request['produto_preco'];
+    public function calculate(array $request)
+    {
+        // Return 0 for Free Shipping, False otherwise.
+        return $this->meetsFreeShippingRequirements($request) ? 0 : false;
+    }
+
+    /**
+    *   Check if current request meets free shipping requirements
+    */
+    private function meetsFreeShippingRequirements(array $request)
+    {
+        if (empty($this->shipping_method->requires)) {
+            return true;
+        } elseif ($this->shipping_method->requires == 'min_amount' || $this->shipping_method->requires == 'either') {
+            if (is_numeric($this->shipping_method->min_amount)) {
+                if (($request['produto_preco'] * $request['quantidade']) > $this->shipping_method->min_amount) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
