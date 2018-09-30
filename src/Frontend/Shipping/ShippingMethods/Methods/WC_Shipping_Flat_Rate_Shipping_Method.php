cf35395b98053574c4a6853622bc279227808dc7
Lucas Bustamante <lucasfbustamante@gmail.com>
2018-09-30 20:31:30 -0300

    Rewrite of Flat Rate Shipping Method

--- src/Frontend/Shipping/ShippingMethods/Methods/WC_Shipping_Flat_Rate_Shipping_Method.php
+++ src/Frontend/Shipping/ShippingMethods/Methods/WC_Shipping_Flat_Rate_Shipping_Method.php
@@ -17,13 +17,17 @@ class WC_Shipping_Flat_Rate_Shipping_Method extends ShippingMethodsAbstract {
     */
     public function calculate(array $request)
     {
+        // Includes WooCommerce Math Class
+        $wc_eval_math_file = WP_PLUGIN_DIR.'/woocommerce/includes/libraries/class-wc-eval-math.php';
+        if (file_exists($wc_eval_math_file))
+            include_once($wc_eval_math_file);
 
         // Get the cost
         $cost = $this->shipping_method->cost;
 
         // WooCommerce stuff
-        $locale         = localeconv();
-        $decimals       = array( wc_get_price_decimal_separator(), $locale['decimal_point'], $locale['mon_decimal_point'], ',' );
+        $locale = localeconv();
+        $decimals = array( wc_get_price_decimal_separator(), $locale['decimal_point'], $locale['mon_decimal_point'], ',' );
 
         // Costs per quantity
         $sum = str_replace('[qty]', $request['quantidade'], $cost);
@@ -38,7 +42,18 @@ class WC_Shipping_Flat_Rate_Shipping_Method extends ShippingMethodsAbstract {
         $sum = rtrim( ltrim( $sum, "\t\n\r\0\x0B+*/" ), "\t\n\r\0\x0B+-*/" );
 
         // Do the math.
-        return $sum ? \WC_Eval_Math::evaluate( $sum ) : false;
+        if (class_exists('\WC_Eval_Math')) {
+            $sum = $sum ? \WC_Eval_Math::evaluate( $sum ) : false;
+        } else {
+            // Let's try one last thing before failing
+            $sum =  is_numeric($sum) ? $sum : false;
+        }
+
+        return array(
+            'price' => $sum,
+            'days' => '-'
+        );
+
     }
 
 
--- src/Frontend/Shipping/ShippingMethods/ShippingMethodsAbstract.php
+++ src/Frontend/Shipping/ShippingMethods/ShippingMethodsAbstract.php
@@ -9,9 +9,6 @@ abstract class ShippingMethodsAbstract
     public function setup($shipping_method) {
         // Stores a copy of the original shipping method class, so we can get costs, etc
         $this->shipping_method = $shipping_method;
-
-        // Includes WooCommerce Math Class
-        require_once(WP_PLUGIN_DIR.'/woocommerce/includes/libraries/class-wc-eval-math.php');
     }
 
     abstract public function getName();
