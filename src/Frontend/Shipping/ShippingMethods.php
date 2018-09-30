8d1e2c600abc731fb034440d08b55f2875fd2661
Lucas Bustamante <lucasfbustamante@gmail.com>
2018-09-30 19:34:11 -0300

    Overhaul of ShippingMethods class

--- src/Frontend/Shipping/ShippingMethods.php
+++ src/Frontend/Shipping/ShippingMethods.php
@@ -8,6 +8,8 @@ class ShippingMethods {
 
     /**
     *   Calculates the shipping costs from the shipping zones provided
+    *
+    *   @todo Provide feedback if $cfpp_shipping_method === false
     */
     public function calculateShippingOptions($shipping_methods, $request)
     {
@@ -16,12 +18,20 @@ class ShippingMethods {
         $factory = new ShippingMethodsFactory;
 
         foreach ($shipping_methods as $shipping_method) {
-            $cfpp_shipping_method = $factory->getClass(get_class($shipping_method));
+            if ($shipping_method->enabled == 'yes') {
+                // Gets the Shipping_Method Class
+                $cfpp_shipping_method = $factory->getClass(get_class($shipping_method));
 
-            if ($cfpp_shipping_method === false)
-                wp_send_json_error('Não foi possível instanciar a classe do Shipping Method: '.get_class($shipping_method));
+                // Makes sure CFPP have Method created for this
+                if ($cfpp_shipping_method === false)
+                    continue;
+                    //wp_send_json_error('Não foi possível instanciar a classe do Shipping Method: '.get_class($shipping_method));
 
-            $shipping_costs[$cfpp_shipping_method->getName()] = $cfpp_shipping_method->calculate($request);
+                // Pass the Shipping Method class to the CFPP Shipping Method
+                $cfpp_shipping_method->setup($shipping_method);
+
+                $shipping_costs[$cfpp_shipping_method->getName()] = $cfpp_shipping_method->calculate($request);
+            }
         }
 
         return $shipping_costs;
