781180cdfd03d90c996bbb89296f97eeaea1d89a
Lucas Bustamante <lucasfbustamante@gmail.com>
2018-09-30 19:32:59 -0300

    Flat rate implementation

--- src/Frontend/Shipping/ShippingMethods/Methods/WC_Shipping_Flat_Rate_Shipping_Method.php
+++ src/Frontend/Shipping/ShippingMethods/Methods/WC_Shipping_Flat_Rate_Shipping_Method.php
@@ -15,8 +15,30 @@ class WC_Shipping_Flat_Rate_Shipping_Method extends ShippingMethodsAbstract {
     /**
     *   Receives a Request and calculates the shipping
     */
-    public function calculate(array $request) {
-        dd($request);
+    public function calculate(array $request)
+    {
+
+        // Get the cost
+        $cost = $this->shipping_method->cost;
+
+        // WooCommerce stuff
+        $locale         = localeconv();
+        $decimals       = array( wc_get_price_decimal_separator(), $locale['decimal_point'], $locale['mon_decimal_point'], ',' );
+
+        // Costs per quantity
+        $sum = str_replace('[qty]', $request['quantidade'], $cost);
+
+        // Remove whitespace from string.
+        $sum = preg_replace( '/\s+/', '', $sum );
+
+        // Remove locale from string.
+        $sum = str_replace( $decimals, '.', $sum );
+
+        // Trim invalid start/end characters.
+        $sum = rtrim( ltrim( $sum, "\t\n\r\0\x0B+*/" ), "\t\n\r\0\x0B+-*/" );
+
+        // Do the math.
+        return $sum ? \WC_Eval_Math::evaluate( $sum ) : false;
     }
 
 
