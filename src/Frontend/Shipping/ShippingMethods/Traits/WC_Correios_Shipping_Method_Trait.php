bb81b59e58cca57e26d2732a6657a6ea910c154e
Lucas Bustamante <lucasfbustamante@gmail.com>
2018-10-10 20:46:25 -0300

    Minor fixes

--- src/Frontend/Shipping/ShippingMethods/Traits/WC_Correios_Shipping_Method_Trait.php
+++ src/Frontend/Shipping/ShippingMethods/Traits/WC_Correios_Shipping_Method_Trait.php
@@ -19,6 +19,7 @@ trait WC_Correios_Shipping_Method_Trait
         $errors[] = $this->checkLength($request['produto_comprimento']);
         $errors[] = $this->checkWeight($request['produto_peso']);
         $errors[] = $this->checkPrice($request['produto_preco']);
+        $errors[] = $this->checkSumHeightWidthLength($request['produto_altura'], $request['produto_largura'], $request['produto_comprimento']);
 
         // Flattens array
         $errors = call_user_func_array('array_merge', $errors);
@@ -87,7 +88,7 @@ trait WC_Correios_Shipping_Method_Trait
         $errors = array();
         if (is_numeric($height) && is_numeric($width) && is_numeric($length)) {
             if ($height + $width + $length > 200) {
-                $errors[] = 'Soma da Altura, Largura e Comprimento ('.$height + $width + $length.') ultrapassa o máximo permitido pelos correios (200cm).';
+                $errors[] = "Soma da Altura, Largura e Comprimento (".$height + $width + $length.") ultrapassa o máximo permitido pelos correios (200cm).";
             }
         }
         return $errors;
@@ -99,11 +100,17 @@ trait WC_Correios_Shipping_Method_Trait
      */
     private function checkWeight($weight)
     {
+        $extra_weight = !empty($this->shipping_method->extra_weight) ? $this->shipping_method->extra_weight: 0;
+
         $errors = array();
         if (!is_numeric($weight)) {
             $errors[] = 'Peso inválido ou não preenchido.';
-        } elseif (is_numeric($weight) && $weight > 30) {
-            $errors[] = 'Peso ('.$weight.'kg) ultrapassa o máximo permitido pelos correios (30kg).';
+        } elseif (is_numeric($weight) && ($weight + $extra_weight) > 30) {
+            if ($extra_weight > 0) {
+                $errors[] = 'Peso ('.$weight.'kg) ultrapassa o máximo permitido pelos correios (30kg). Considerando peso extra configurado do método de entrega, que é de '.$extra_weight.'kg';
+            } else {
+                $errors[] = 'Peso ('.$weight.'kg) ultrapassa o máximo permitido pelos correios (30kg).';
+            }
         }
         return $errors;
     }
@@ -114,7 +121,17 @@ trait WC_Correios_Shipping_Method_Trait
      */
     private function checkPrice($price)
     {
-        $maximum_price = $this->shipping_method->id == 'correios-pac' ? 3000 : 10000;
+        switch ($this->shipping_method->id) {
+            case 'correios-pac':
+                $maximum_price = 3000;
+                break;
+            case 'correios-sedex':
+                $maximum_price = 10000;
+                break;
+            default:
+                $maximum_price = PHP_INT_MAX;
+                break;
+        }
         $errors = array();
         if (!is_numeric($price)) {
             $errors[] = 'Preço inválido ou não preenchido. ('.$price.')';
