3325e6769986a6a2e8ffcbc807e0e5a4f1ac834a
Lucas Bustamante <lucasfbustamante@gmail.com>
2019-01-10 23:44:50 -0200

    Melhorando mensagem de erro para exception de Package

--- src/Exceptions/PackageException.php
+++ src/Exceptions/PackageException.php
@@ -13,6 +13,6 @@ class PackageException extends \Exception
      */
     public static function invalid_package()
     {
-        return new self(__('Could not generate a valid Package for Payload object.', 'woo-correios-calculo-de-frete-na-pagina-do-produto'), 501);
+        return new self(__('Could not validate product dimensions. Check if product width, height, length and weight are valid.', 'woo-correios-calculo-de-frete-na-pagina-do-produto'), 501);
     }
 }
