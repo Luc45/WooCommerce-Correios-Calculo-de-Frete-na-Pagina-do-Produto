e3be0d1335e686ca240ff12db41c51166cda2290
Lucas Bustamante <lucasfbustamante@gmail.com>
2021-03-14 13:30:02 -0300

    composer install --no-dev

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,24 +50,6 @@ class ComposerAutoloaderInit48df958508994a128baa766dcdb046c6
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit48df958508994a128baa766dcdb046c6::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire48df958508994a128baa766dcdb046c6($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire48df958508994a128baa766dcdb046c6($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
